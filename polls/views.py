beac98a2650de47d398a51c721f9b2da81901955
pimon0807 <k.arkw87@gmail.com>
2021-12-04 15:22:41 +0900

    汎用ビューに変更

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.template import loader
+from django.views import generic
 
 from .models import Question, Choice
 
-
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -23,6 +22,21 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+'''
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
